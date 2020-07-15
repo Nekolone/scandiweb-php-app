b8d26f0a7ebd866e00b798119942df7efb439160
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-07-15 19:17:09 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS shopdb;
 CREATE DATABASE shopdb;
 USE shopdb;
 
-CREATE TABLE Product as P(
+CREATE TABLE Product as P (
   SKU varchar(8),
   itemName varchar(255),
   addTime timestamp,
@@ -10,19 +10,19 @@ CREATE TABLE Product as P(
   itemType int,
 );
 
-CREATE TABLE TypeSize as TS(
+CREATE TABLE TypeSize as TS (
   SKU varchar(8),
   itemSize int,
  );
  
-CREATE TABLE TypeHWL as THWL(
+CREATE TABLE TypeHWL as THWL (
   SKU varchar(8),
   itemHeight int,
   itemWidth int,
   itemLength int 
 );
 
-CREATE TABLE TypeWeigth as TW(
+CREATE TABLE TypeWeigth as TW (
   SKU varchar(8),
   itemWeight int
 );
