33174338e1b144a4665af3c5f7b8fa0ca55bc555
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-07-15 19:12:13 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,27 +2,27 @@ DROP DATABASE IF EXISTS shopdb;
 CREATE DATABASE shopdb;
 USE shopdb;
 
-CREATE TABLE Product{
+CREATE TABLE Product as P(
   SKU varchar(8),
   itemName varchar(255),
   addTime timestamp,
   price decimal,
   itemType int,
-};
+);
 
-CREATE TABLE TypeSize{
+CREATE TABLE TypeSize as TS(
   SKU varchar(8),
   itemSize int,
- };
+ );
  
-CREATE TABLE TypeHWL{
+CREATE TABLE TypeHWL as THWL(
   SKU varchar(8),
   itemHeight int,
   itemWidth int,
   itemLength int 
-};
+);
 
-CREATE TABLE TypeWeigth{
+CREATE TABLE TypeWeigth as TW(
   SKU varchar(8),
   itemWeight int
-};
+);
