603df49c1914b3cac8343c719165f424f0bfcc03
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-07-18 19:06:27 +0300

    update 0.1.7 type switcher

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
+function run() {
+    var e = document.getElementById("item__type");
+    var n = e.options[e.selectedIndex].value;
 
-});
+    switch (n) {
+        case "0":
+            $('.size').css("display", "block");
+            $('.HWL').css("display", "none");
+            $('.weight').css("display", "none");
+            break;
+        case "1":
+            $('.size').css("display", "none");
+            $('.HWL').css("display", "block");
+            $('.weight').css("display", "none");
+            break;
+        case "2":
+            $('.size').css("display", "none");
+            $('.HWL').css("display", "none");
+            $('.weight').css("display", "block");
+            break;
+    }
+}
