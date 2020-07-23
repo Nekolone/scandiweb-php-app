5cedd94e67c27e633c07fe1f90780a49b681e21e
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-07-23 22:49:05 +0300

    update 0.1.9 IT WORKS!!!!!!!!!!!!

--- project/support_files/InformationProvider.php
+++ project/support_files/InformationProvider.php
@@ -1,74 +1,117 @@
 <?php
 
-
-class InformationProvider
+abstract class Product
 {
     public $SKU;
     public $name;
     public $price;
-    public $image_link;
+    public $image__link;
     public $item__type;
+
+    abstract public function outputInfo();
+
+}
+
+
+class SizeProduct extends Product
+{
     public $size;
+
+    function __construct($SKU)
+    {
+
+        $mysqli = msl();
+        $mysqli->query("SET NAMES 'utf8'");
+        $getRow = $mysqli->query("SELECT * FROM `Product` JOIN TypeSize  on Product.SKU = TypeSize.SKU WHERE Product.SKU='$SKU'");
+        $row = mysqli_fetch_assoc($getRow);
+        $mysqli->close();
+
+        $this->SKU = $row["SKU"];
+        $this->name = $row["name"];
+        $this->price = $row["price"];
+        $this->image__link = $row["image__link"];
+        $this->item__type = $row["item__type"];
+        $this->size = $row["size"];
+    }
+
+    public function outputInfo()
+    {
+        echo "<div class=\"info__box\">";
+        echo "<a href=\"#\"><img src=\"" . $this->image__link . "\" alt=\"pic\"></a>";
+        echo "<div><table><tbody><tr><td><span>Name:</span></td><td><span>" . $this->name . "</span></td>";
+        echo "</tr><tr><td><span>Price:</span></td><td><span>" . $this->price . "</span></td>";
+        echo "</tr><tr><td><span>Attribute:</span></td><td><span>size=" . $this->size . "</span></td>";
+        echo "</tr><tr><td><span>SKU:</span></td><td><span>" . $this->SKU . "</span></td>";
+        echo "</tr></tbody></table></div></div>";
+    }
+
+}
+
+class DimensionalProduct extends Product
+{
     public $height;
     public $width;
     public $length;
+
+    function __construct($SKU)
+    {
+        $mysqli = msl();
+        $mysqli->query("SET NAMES 'utf8'");
+        $getRow = $mysqli->query("SELECT * FROM `Product` JOIN TypeHWL on Product.SKU = TypeHWL.SKU WHERE Product.SKU='$SKU'");
+        $row = mysqli_fetch_assoc($getRow);
+        $mysqli->close();
+
+        $this->SKU = $row["SKU"];
+        $this->name = $row["name"];
+        $this->price = $row["price"];
+        $this->image__link = $row["image__link"];
+        $this->item__type = $row["item__type"];
+        $this->height = $row["height"];
+        $this->width = $row["width"];
+        $this->length = $row["length"];
+    }
+
+    public function outputInfo()
+    {
+        echo "<div class=\"info__box\">";
+        echo "<a href=\"#\"><img src=\"" . $this->image__link . "\" alt=\"pic\"></a>";
+        echo "<div><table><tbody><tr><td><span>Name:</span></td><td><span>" . $this->name . "</span></td>";
+        echo "</tr><tr><td><span>Price:</span></td><td><span>" . $this->price . "</span></td>";
+        echo "</tr><tr><td><span>Attribute:</span></td><td><span>HxWxL=" . $this->height . "x" . $this->width . "x" . $this->length . "</span></td>";
+        echo "</tr><tr><td><span>SKU:</span></td><td><span>" . $this->SKU . "</span></td>";
+        echo "</tr></tbody></table></div></div>";
+    }
+}
+
+class WeightProduct extends Product
+{
     public $weight;
 
-    function __construct($SKU, $name, $price, $image_link, $item__type, $size, $height, $width, $length, $weight)
+    function __construct($SKU)
     {
-        $this->SKU = $SKU;
-        $this->name = $name;
-        $this->price = $price;
-        $this->image_link = $image_link;
-        $this->item__type = $item__type;
+        $mysqli = msl();
+        $mysqli->query("SET NAMES 'utf8'");
+        $getRow = $mysqli->query("SELECT * FROM `Product` JOIN TypeWeight  on Product.SKU = TypeWeight.SKU WHERE Product.SKU='$SKU'");
+        $row = mysqli_fetch_assoc($getRow);
+        $mysqli->close();
 
-        switch ($item__type) {
-            case "0":
-                $this->size = $size;
-                break;
-            case "1":
-                $this->height = $height;
-                $this->width = $width;
-                $this->length = $length;
-                break;
-            case "2":
-                $this->weight = $weight;
-                break;
-        }
+        $this->SKU = $row["SKU"];
+        $this->name = $row["name"];
+        $this->price = $row["price"];
+        $this->image__link = $row["image__link"];
+        $this->item__type = $row["item__type"];
+        $this->weight = $row["weight"];
 
     }
 
-    public function dbConnect()
+    public function outputInfo()
     {
-        $host = 'localhost:8085';
-        $db = 'shopdb';
-        $user = 'admin';
-        $pass = 'yf9K7ItvSRlC';
-        $charset = 'utf8';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $opt = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        $pdo = new PDO($dsn, $user, $pass, $opt);
+        echo "<div class=\"info__box\">";
+        echo "<a href=\"#\"><img src=\"" . $this->image__link . "\" alt=\"pic\"></a>";
+        echo "<div><table><tbody><tr><td><span>Name:</span></td><td><span>" . $this->name . "</span></td>";
+        echo "</tr><tr><td><span>Price:</span></td><td><span>" . $this->price . "</span></td>";
+        echo "</tr><tr><td><span>Attribute:</span></td><td><span>weight=" . $this->weight . "</span></td>";
+        echo "</tr><tr><td><span>SKU:</span></td><td><span>" . $this->SKU . "</span></td>";
+        echo "</tr></tbody></table></div></div>";
     }
-
-    public function add_type_0($SKU, $name, $price, $image_link, $item__type, $size)
-    {
-        dbConnect();
-    }
-
-    public function add_type_1($SKU, $name, $price, $image_link, $item__type, $height, $width, $length)
-    {
-
-    }
-
-    public function add_type_2($SKU, $name, $price, $image_link, $item__type, $weight)
-    {
-
-    }
-
-
 }
