4411c78cec37758158e4a0ddfb9e8e4cc8d8ba7e
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-07-27 15:27:04 +0300

    add new method to data accessor

--- project/support_files/DataAccessService.php
+++ project/support_files/DataAccessService.php
@@ -6,11 +6,20 @@ class DataAccessService
     private $mysqli;
     private static $instance = null;
 
-    public function getSingleRow($query)
+    public function getSingleResultFromQuery($query)
     {
         $getRow = $this->mysqli->query($query);
+        if ($getRow->num_rows > 1) {
+            throw new LengthException("More than one row returned for query:$query");
+        }
         return mysqli_fetch_assoc($getRow);
     }
+
+    public function getQueryResults($query)
+    {
+        return $this->mysqli->query($query);
+    }
+
 
     public static function getDataAccessor()
     {
