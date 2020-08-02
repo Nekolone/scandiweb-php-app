8d8f1eb0460ca51f537361308239bfc7f3485198
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-08-02 23:31:49 +0300

    smell fixes again

--- project/main.php
+++ project/main.php
@@ -26,28 +26,6 @@ isset($_POST["done"])?header("Location:search_list.php?search=".$_POST["search"]
                     <a href="#">
                         <img src="https://sun1-21.userapi.com/zRvW6840WZKmcBKkUblIFCYM-SA698L2SSXh6w/N0DwuwKHmtM.jpg"
                              alt="pic"></a>
-                    <div>
-                        <table>
-                            <tbody>
-                            <tr>
-                                <td><span>Name:</span></td>
-                                <td><span>Name</span></td>
-                            </tr>
-                            <tr>
-                                <td><span>Price:</span></td>
-                                <td><span>250Eur</span></td>
-                            </tr>
-                            <tr>
-                                <td><span>Attribute:</span></td>
-                                <td><span>250x145x230</span></td>
-                            </tr>
-                            <tr>
-                                <td><span>SKU:</span></td>
-                                <td><span>123456</span></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
                 <div class="test__box">1</div>
                 <div class="test__box">1</div>
