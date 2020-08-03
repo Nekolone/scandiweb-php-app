7d1963232948217f52bfeaae12bb6e6be6d8e0fe
Nekolone <nikolafgh@gmail.com>
2020-08-03 23:43:27 +0300

    add sass

--- project/header.php
+++ project/header.php
@@ -8,7 +8,7 @@ require'validation.php';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../resources/css/main.css">
+    <link rel="stylesheet" href="../resources/css/main.sass">
     <script src="../resources/js/jquery-3.5.1.min.js"></script>
     <script src="../resources/js/main.js"></script>
     <title><?=$title?></title>
