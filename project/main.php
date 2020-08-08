0f9a3d5ed6195982d9b167c6f6f3020300553603
Nekolone <60545501+Nekolone@users.noreply.github.com>
2020-08-08 22:41:00 +0300

    sass update

--- project/main.php
+++ project/main.php
@@ -6,7 +6,7 @@ include_once "support_files/list_db.php";
 isset($_POST["done"])?header("Location:search_list.php?search=".$_POST["search"]):false;
 ?>
 <form action="" method="post">
-    <input type="text" name="search" placeholder="<?=$_GET["search"]?>"><br>
+    <input type="text" class="search__line" name="search" placeholder="<?=$_GET["search"]?>"><br>
     <input type="submit" class="button" name="done" value="SEARCH">
 </form>
 
@@ -21,12 +21,9 @@ isset($_POST["done"])?header("Location:search_list.php?search=".$_POST["search"]
             <div class="item__box">
                 <?php
                  listResult();
+
                 ?>
-                <div class="info__box">
-                    <a href="#">
-                        <img src="https://sun1-21.userapi.com/zRvW6840WZKmcBKkUblIFCYM-SA698L2SSXh6w/N0DwuwKHmtM.jpg"
-                             alt="pic"></a>
-                </div>
+
                 <div class="test__box">1</div>
                 <div class="test__box">1</div>
                 <div class="test__box">1</div>
